bb713b2ffb14fbba330184c267c10b064a5ae61d
Al-Capone11 <joseman2004@hotmail.com>
2026-05-28 22:12:43 -0500

    Pose watcher legs beta7

--- fabric/src/client/java/com/josem/thewatcher/client/TheWatcherModel.java
+++ fabric/src/client/java/com/josem/thewatcher/client/TheWatcherModel.java
@@ -30,10 +30,10 @@ public final class TheWatcherModel<T extends TheWatcherEntity> extends EntityMod
         watcher.addOrReplaceChild("left_lower_arm", CubeListBuilder.create().texOffs(34, 31).addBox(6.5F, -21.0F, -0.5F, 1.0F, 9.0F, 1.0F), PartPose.ZERO);
         watcher.addOrReplaceChild("right_upper_arm", CubeListBuilder.create().texOffs(26, 31).addBox(-8.0F, -30.0F, -1.0F, 2.0F, 9.0F, 2.0F), PartPose.ZERO);
         watcher.addOrReplaceChild("right_lower_arm", CubeListBuilder.create().texOffs(0, 35).addBox(-7.5F, -21.0F, -0.5F, 1.0F, 9.0F, 1.0F), PartPose.ZERO);
-        watcher.addOrReplaceChild("left_upper_leg", CubeListBuilder.create().texOffs(31, 0).addBox(1.0F, -19.0F, -2.0F, 2.0F, 10.0F, 3.0F), PartPose.ZERO);
-        watcher.addOrReplaceChild("left_lower_leg", CubeListBuilder.create().texOffs(4, 35).addBox(1.5F, -9.0F, -0.5F, 1.0F, 10.0F, 1.0F), PartPose.ZERO);
-        watcher.addOrReplaceChild("right_upper_leg", CubeListBuilder.create().texOffs(31, 10).addBox(-3.0F, -19.0F, -2.0F, 2.0F, 10.0F, 3.0F), PartPose.ZERO);
-        watcher.addOrReplaceChild("right_lower_leg", CubeListBuilder.create().texOffs(8, 35).addBox(-2.5F, -9.0F, -0.5F, 1.0F, 10.0F, 1.0F), PartPose.ZERO);
+        watcher.addOrReplaceChild("left_upper_leg", CubeListBuilder.create().texOffs(31, 0).addBox(1.0F, -19.0F, -1.0F, 2.0F, 10.0F, 3.0F), PartPose.ZERO);
+        watcher.addOrReplaceChild("left_lower_leg", CubeListBuilder.create().texOffs(4, 35).addBox(1.5F, -9.0F, -2.5F, 1.0F, 10.0F, 1.0F), PartPose.ZERO);
+        watcher.addOrReplaceChild("right_upper_leg", CubeListBuilder.create().texOffs(31, 10).addBox(-3.0F, -19.0F, -1.0F, 2.0F, 10.0F, 3.0F), PartPose.ZERO);
+        watcher.addOrReplaceChild("right_lower_leg", CubeListBuilder.create().texOffs(8, 35).addBox(-2.5F, -9.0F, -2.5F, 1.0F, 10.0F, 1.0F), PartPose.ZERO);
 
         return LayerDefinition.create(mesh, 64, 64);
     }
