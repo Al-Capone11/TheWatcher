8a7c5b54a702518b6b193c5d164c400e7cb229f3
Al-Capone11 <joseman2004@hotmail.com>
2026-06-02 19:42:28 -0500

    Force watcher visual facing beta11

--- fabric/src/client/java/com/josem/thewatcher/client/TheWatcherRenderer.java
+++ fabric/src/client/java/com/josem/thewatcher/client/TheWatcherRenderer.java
@@ -2,9 +2,13 @@ package com.josem.thewatcher.client;
 
 import com.josem.thewatcher.TheWatcherMod;
 import com.josem.thewatcher.entity.TheWatcherEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 
 public final class TheWatcherRenderer extends LivingEntityRenderer<TheWatcherEntity, TheWatcherModel<TheWatcherEntity>> {
     private static final ResourceLocation SKIN = TheWatcherMod.id("textures/entity/thewatcher.png");
@@ -21,6 +25,20 @@ public final class TheWatcherRenderer extends LivingEntityRenderer<TheWatcherEnt
     @Override
     protected boolean shouldShowName(TheWatcherEntity entity) {
         return false;
+    }
+
+    @Override
+    protected void setupRotations(TheWatcherEntity entity, PoseStack poseStack, float ageInTicks, float rotationYaw, float partialTicks) {
+        Player player = Minecraft.getInstance().player;
+        if (player == null) {
+            super.setupRotations(entity, poseStack, ageInTicks, rotationYaw, partialTicks);
+            return;
+        }
+
+        double dx = player.getX() - entity.getX();
+        double dz = player.getZ() - entity.getZ();
+        float yawToPlayer = (float) Math.toDegrees(Math.atan2(dz, dx)) - 90.0F;
+        poseStack.mulPose(Axis.YP.rotationDegrees(180.0F - yawToPlayer));
     }
 }
 
